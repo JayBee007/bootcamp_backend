e4e78bc3d52da322c96c65f2435f1ce7e6b2ecae
Jay <jaybee.codes@gmail.com>
2017-01-14 16:46:19 +0000

    title added

--- npm_exercise/listProducts.js
+++ npm_exercise/listProducts.js
@@ -1,5 +1,6 @@
 var faker = require("faker");
 
+console.log("============================\nWelcome to My Shop\n============================")
 for(var i = 0; i<10; i++){
     console.log(faker.fake("{{commerce.productName}} - {{commerce.price}}$"))
 }
