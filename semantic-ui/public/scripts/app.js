5a362ef2a7000308d3eabc0b1340ace397975ad9
Jay <jaybee.codes@gmail.com>
2017-02-04 15:52:33 +0000

    add dimmer on image hover

--- semantic-ui/public/scripts/app.js
+++ semantic-ui/public/scripts/app.js
@@ -2,7 +2,11 @@ $("document").ready(function(){
    var rel = $('body[rel]').attr('rel');
    var menu = $("#menu a");
    menu.eq(rel).addClass("active");
+   
    $("#m_btn").on("click",function(){
       $("#m_menu").sidebar('toggle'); 
    });
+   
+   $(".ui.small.circular.image").dimmer({on: "hover"});
+   
 });
