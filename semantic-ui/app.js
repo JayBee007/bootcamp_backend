c865cdef652d690659bbeab328158a9c723f23ac
Jay <jaybee.codes@gmail.com>
2017-02-04 13:45:56 +0000

    add active link

--- semantic-ui/app.js
+++ semantic-ui/app.js
@@ -7,6 +7,20 @@ app.get("/", function(req,res){
    res.render("index.ejs"); 
 });
 
+app.get("/about", function(req,res){
+   res.render("about.ejs"); 
+});
+
+app.get("/portfolio", function(req,res){
+   res.render("portfolio.ejs"); 
+});
+
+app.get("/contact", function(req,res){
+   res.render("contact.ejs"); 
+});
+
+
+
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("serving"); 
 });
--- semantic-ui/public/scripts/app.js
+++ semantic-ui/public/scripts/app.js
@@ -1,4 +1,7 @@
 $("document").ready(function(){
+   var rel = $('body[rel]').attr('rel');
+   var menu = $("#menu a");
+   menu.eq(rel).addClass("active");
    $("#m_btn").on("click",function(){
       $("#m_menu").sidebar('toggle'); 
    });
