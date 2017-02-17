20209ebb2e877889bde5093224082ebc7ede3ae0
Jay <jaybee.codes@gmail.com>
2017-02-17 18:32:59 +0000

    create cat_app db

--- database/cats.js
+++ database/cats.js
@@ -5,9 +5,57 @@ mongoose.connect("mongodb://localhost/cat_app");
 var catSchema = new mongoose.Schema({
     name: String,
     age: Number,
-    temperament:String
+    temperament: String
 });
+
+var Cat = mongoose.model("Cat", catSchema);
+
 // add item to databse
+// var george = new Cat({
+//     name: "George",
+//     age:11,
+//     temperament: "Grouchy"
+// });
+
+// george.save(function(err, item) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log("cat saved");
+//         console.log(item);
+
+//     }
+// });
 
 
 // retrieve item from database and console.log each one
+Cat.find({}, function(err, results) {
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log(results);
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
