753d96c6b7eb10ce8b1f9f600ef995df924bfc9f
Jay <jaybee.codes@gmail.com>
2017-03-19 12:32:57 +0000

    created models directory

--- associations/references.js
+++ associations/references.js
@@ -1,62 +1,44 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_reference");
 
-// Post - title, content
-var postSchema = new mongoose.Schema({
-   title:String,
-   content:String
+var Post = require("./models/post");
+
+var User = require("./models/user");
+
+Post.create({
+  title:"How to cook",
+  content:"Kitty power! chase the red dot, hairball run catnip eat the grass sniff. Please stop looking at your phone and pet me meow for food, then when human fills food dish, take a few bites of food and continue meowing hide when guests come over hate dog. Intrigued by the shower cats go for world domination refuse to drink water except out of someone's glass purrrrrr wake up human for food at 4am lick yarn hanging out of own butt. Scream for no reason at 4 am."
+}, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        User.findOne({email: "bob@gmail.com"}, function(err,foundUser){
+          if(err){
+              console.log(err);
+          }else {
+              foundUser.posts.push(post);
+              foundUser.save(function(err,data){
+                  if(err){
+                      console.log(err);
+                  }else {
+                      console.log(data);
+                  }
+              })
+          }
+        });
+    }
 });
-
-var Post = mongoose.model("Post", postSchema);
-
-// User - email, name
-var userSchema = new mongoose.Schema({
-    email:String,
-    name:String,
-    posts:[
-            {
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"Post"
-            }
-        ]
-});
-    
-var User = mongoose.model("User", userSchema);
-
-// Post.create({
-//   title:"How to cook  burger",
-//   content:"Kitty power! i tell my human for hide from vacuum cleaner or kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff. Please stop looking at your phone and pet me meow for food, then when human fills food dish, take a few bites of food and continue meowing hide when guests come over hate dog. Intrigued by the shower cats go for world domination refuse to drink water except out of someone's glass purrrrrr wake up human for food at 4am lick yarn hanging out of own butt. Scream for no reason at 4 am."
-// }, function(err, post){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         User.findOne({email: "bob@gmail.com"}, function(err,foundUser){
-//           if(err){
-//               console.log(err);
-//           }else {
-//               foundUser.posts.push(post);
-//               foundUser.save(function(err,data){
-//                   if(err){
-//                       console.log(err);
-//                   }else {
-//                       console.log(data);
-//                   }
-//               })
-//           }
-//         });
-//     }
-// });
 
 // Find user
 // Find all posts for that user
 
-User.findOne({email:"bob@gmail.com"}).populate("posts").exec(function(err,user){
-   if(err){
-       console.log(err);
-   }else {
-       console.log(user);
-   }
-});
+// User.findOne({email:"bob@gmail.com"}).populate("posts").exec(function(err,user){
+//   if(err){
+//       console.log(err);
+//   }else {
+//       console.log(user);
+//   }
+// });
 
 
 // User.create({
